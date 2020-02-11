1fa1c37d85cea7aac4cd185f1dd34d059110ae7f
Maxim1erDAM <57709446+Maxim1erDAM@users.noreply.github.com>
2020-02-11 09:45:10 +0100

    Create testdeclases.java

---
+++ testdeclases.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication56;
+
+/**
+ *
+ * @author 1erDAM
+ */
+public class JavaApplication56 {
+
+    class Alumne {
+
+        String nom;
+        String cognoms;
+        int edat;
+        float nota;
+    }
+
+    class Data {
+
+        int dia;
+        int mes;
+        int any;
+    }
+
+    class Temps {
+
+        int hores;
+        int minuts;
+        int segons;
+        int centessimes;
+    }
+
+    class Rectangle {
+
+        int x1, y1; // Coordenades punt inferior-esquerra
+        int x2, y2; // Coordenades punt superior-dreta
+    }
+
+    class Concursant {
+
+        String nomComplet;  // Nom i cognoms
+        String nomArtistic; // El nom artístic del concursant
+        int any;         // Any de naixement
+    }
+
+    class CDAudio {
+
+        String grup;
+        String titol;
+        int any;
+        int qcançons;
+    }
+
+    class NomComplet {
+
+        String nom;
+        String cog1;
+        String cog2;
+    }
+
+    class TelFix {
+
+        int prefix;
+        int numero;
+    }
+
+    class Domicili {
+
+        String carrer;
+        int numero, pis, porta, cpostal;
+        String poblacio;
+        String comarca;
+    }
+
+    class Color {
+
+        int roig;
+        int verd;
+        int blau;
+    }
+
+/**
+ * @param args the command line arguments
+ */
+public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
