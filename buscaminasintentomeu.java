b2be65463ef696e4a4037a6b853b351d4f5175da
Maxim1erDAM <57709446+Maxim1erDAM@users.noreply.github.com>
2020-03-02 13:14:24 +0100

    BUSCAMINES INTENTO

---
+++ buscaminasintentomeu.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buscaminesfinal;
+
+import java.util.*;
+
+/**
+ *
+ * @author 1erDAM
+ */
+public class BuscaMinesFinal {
+    private static final String TAPAT = "X";
+    private static final String BANDERETA = "P";
+    private static final String RES = "_";
+    private static final String SEPARADOR = "|";
+
+
+
+    private static int[][] Tablero; 
+    private static boolean[][] TableroMinas; 
+    private static int[][] TableroDescubierto; 
+
+    
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+    }
+    
+    
+    public static int minar() {
+        
+        
+        return MINESPOSADES;
+    }
+    
+    
+    minat(int f, int c)
+    {
+        
+        
+        
+    Boolean minaono;
+    
+    return minaono;
+    
+    
+    }
+    
+    incorrecte(int f, int c){
+        
+    Boolean poscorrectaono;
+    
+    return poscorrectaono;
+    
+    }
+                
+    qma(int f, int c) {
+        int quantitatmines;
+        return quantitatmines;
+        
+    }
+    
+    destapat(int f, int c ) {
+        
+        boolean posdestapada;
+        return posdestapada;
+        
+    }
+            
+    qdestapats(){
+        int celdesdestapades;
+        
+        return celdesdestapades;
+
+    }
+            
+    mostrarTauler(boolean m){
+        
+    }
+    
+    
+    picar(int f, int c){
+        boolean picadaono;
+        
+        
+    }
+}
